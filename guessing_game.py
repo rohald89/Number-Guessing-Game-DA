ab677ecdae73ffcfbde6af3f21db35972ca4c47b
Rohald van Merode <rohald_89@live.nl>
2022-07-13 11:21:46 +0200

    added difficulty levels

--- guessing_game.py
+++ guessing_game.py
@@ -1,24 +1,28 @@
 import random
 
 
-def start_game(high_score=100):
+def start_game(high_score=None):
 
-    print(f"""
-Welcome to the number guessing game!
-You have to guess a number between 1 and 100.
-The current high score is {high_score}.
-""")
+    print("Welcome to the number guessing game!")
 
     tries = 0
-    answer = random.randint(1, 100)
+    max_number = get_difficulty()
+
+    print(f"""
+You have to guess a number between 1 and {max_number}.
+The current high score is {high_score or max_number}.
+""")
+
+    answer = random.randint(1, max_number)
+
     while True:
         try:
-            guess = int(input("Guess a number between 1 and 100: "))
+            guess = int(input(f"Guess a number between 1 and {max_number}: "))
         except ValueError:
             print("Please enter a number.")
             continue
-        if guess < 1 or guess > 100:
-            print("Please enter a number between 1 and 100.")
+        if guess < 1 or guess > max_number:
+            print(f"Please enter a number between 1 and {max_number}.")
             continue
         else:
             tries += 1
@@ -30,7 +34,7 @@ The current high score is {high_score}.
                 continue
             else:
                 print(f"You got it! It took you {tries} tries.")
-                if tries < high_score:
+                if high_score == None or tries < high_score:
                     high_score = tries
                     print(f"You set a new high score of {high_score} tries!")
                 break
@@ -49,6 +53,25 @@ def restart_game(high_score):
         else:
             print("Please enter a valid answer.")
             continue
+
+
+def get_difficulty():
+    max_number = 100
+
+    difficulty = input("Please choose a difficulty level (easy/medium/hard): ")
+    if difficulty.lower() == "easy":
+        max_number = 10
+    elif difficulty.lower() == "medium":
+        max_number = 100
+    elif difficulty.lower() == "hard":
+        max_number = 1000
+    elif difficulty.lower() == "impossible":
+        print("You asked for it... 🤷")
+        max_number = 10000
+    else:
+        print("Please enter a valid difficulty level.")
+        get_difficulty()
+    return max_number
 
 
 start_game()
